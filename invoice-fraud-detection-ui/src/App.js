e69b504a3b4e9fb1357ff649654b91aa38a159c9
BojanMarinkovski <bojanmarinkovski777@gmail.com>
2025-02-15 20:37:21 +0200

    frontend initial configuration

---
+++ invoice-fraud-detection-ui/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Invoice Fraud Detection
+    </div>
+  );
+}
+
+export default App;
