59c65fc7b4c03d86e0a91e368e087feac0e42abc
BojanMarinkovski <bojanmarinkovski777@gmail.com>
2025-02-15 20:28:47 +0200

    implement backend initial configuration

---
+++ invoice-fraud-detection-be/src/main/java/com/invoicefraud/InvoiceFraudDetectionBeApplication.java
@@ -0,0 +1,13 @@
+package com.invoicefraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceFraudDetectionBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceFraudDetectionBeApplication.class, args);
+    }
+
+}
---
+++ invoice-fraud-detection-be/src/test/java/com/invoicefraud/InvoiceFraudDetectionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoicefraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceFraudDetectionBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
