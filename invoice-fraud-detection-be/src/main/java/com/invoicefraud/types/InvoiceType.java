d4dc9559cc97a95c4163b3f1ecefe2b303efa95d
BojanMarinkovski <marinkovski3108@gmail.com>
2025-02-16 11:31:09 +0200

    add @JsonValue on InvoiceType enum

--- invoice-fraud-detection-be/src/main/java/com/invoicefraud/types/InvoiceType.java
+++ invoice-fraud-detection-be/src/main/java/com/invoicefraud/types/InvoiceType.java
@@ -1,5 +1,6 @@
 package com.invoicefraud.types;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 @Getter
@@ -8,6 +9,7 @@ public enum InvoiceType {
     OK("OK"),
     SUSPICIOUS("Suspicious");
 
+    @JsonValue
     private final String description;
 
     InvoiceType(String description) {
